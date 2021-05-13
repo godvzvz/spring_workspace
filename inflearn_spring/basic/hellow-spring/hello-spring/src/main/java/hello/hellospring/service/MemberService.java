3816eba890606bb88f5e27e015f5fd2b5ad0c651
sang-jun lee <godvzvz@gmail.com>
2021-05-13 16:26:13 +0900

    4강 의존주입

--- inflearn_spring/basic/hellow-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ inflearn_spring/basic/hellow-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -1,15 +1,19 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
-    public final MemoryMemberRepository repository;
-
-    public MemberService(MemoryMemberRepository repository) {
+    public final MemberRepository repository;
+    @Autowired
+    public MemberService(MemberRepository repository) {
         this.repository = repository;
     }
 
