0c8081673f77034d637bddfed5cba48b65c85484
sang-jun lee <godvzvz@gmail.com>
2021-05-18 16:32:33 +0900

    5강 화면 만들기

---
+++ inflearn_spring/basic/hellow-spring/hello-spring/src/main/java/hello/hellospring/controller/HomeControleer.java
@@ -0,0 +1,12 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeControleer {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
