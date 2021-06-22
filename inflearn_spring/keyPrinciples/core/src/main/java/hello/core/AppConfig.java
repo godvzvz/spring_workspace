2888460edf334a3ecb3d64efb6a70f782818c6b8
sang-jun lee <godvzvz@gmail.com>
2021-06-22 21:34:58 +0900

    5강 BeanFactory, ApplicationContext, BeanDefinition 설명

--- inflearn_spring/keyPrinciples/core/src/main/java/hello/core/AppConfig.java
+++ inflearn_spring/keyPrinciples/core/src/main/java/hello/core/AppConfig.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+
     @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
